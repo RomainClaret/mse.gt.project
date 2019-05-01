d7e23ab8faa360b3d776aea3a08960cb684a1f44
Romain Claret <romain.claret@rocla.ch>
2019-05-01 02:16:22 +0200

    + bullet particle

--- platformer/scripts/Bullet.gd
+++ platformer/scripts/Bullet.gd
@@ -3,15 +3,18 @@ extends Area
 
 var SPEED = 30
 var velocity = Vector3()
+var bullet_fly = true
+var previous_velocity= Vector3()
 
 
 func shoot(xform):
 	transform = xform
 	velocity = -transform.basis.z * SPEED
-
+	
 
 func _process(delta):
-	transform.origin += velocity * delta
+	if bullet_fly:
+		transform.origin += velocity * delta
 
 func _on_Timer_timeout():
 	queue_free()
@@ -19,4 +22,11 @@ func _on_Timer_timeout():
 
 func _on_Bullet_body_entered(body):
 	if body is StaticBody:
+		get_node("Particles").show()
+		bullet_fly = false
+		#velocity = -transform.basis.z * 0
+		#transform.origin -= previous_velocity
+		
+		yield(get_tree().create_timer(1.0), "timeout")
 		queue_free()
+		
